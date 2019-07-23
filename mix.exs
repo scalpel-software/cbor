b83d85e02a77cc53a425cb41f82279ea858fe1c9
Thomas Cioppettini <tom@tecrails.com>
2019-07-22 20:43:05 -0400

    chore(LICENSE): Prepare the package for generate release by adding the MIT license and ex_doc dependency

--- mix.exs
+++ mix.exs
@@ -7,6 +7,12 @@ defmodule Cbor.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
+      description: "Implementation of RFC 7049 (Concise Binary Object Representation)",
+      package: [
+        maintainers: ["tomciopp"],
+        licenses: ["MIT"],
+        links: %{"GitHub" => "https://github.com/scalpel-software/cbor"}
+      ],
       deps: deps()
     ]
   end
@@ -21,8 +27,7 @@ defmodule Cbor.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false}
     ]
   end
 end
